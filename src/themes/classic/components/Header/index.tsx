edbedea28e956a13eed2b831ebf89d8c5d187b00
moyu800 <42370847+moyu800@users.noreply.github.com>
2026-08-08 18:23:16 +0800

    Hide header logo when none is configured

--- src/themes/classic/components/Header/index.tsx
+++ src/themes/classic/components/Header/index.tsx
@@ -54,13 +54,15 @@ const Header = () => {
   return (
     <>
       <nav className="mx-auto mt-12 flex w-full max-w-screen-2xl min-w-max items-center justify-between pl-6 lg:px-16">
-        <div className="w-1/4">
-          <Link to={siteUrl}>
-            <picture>
-              <img className="h-16 w-16 rounded-full" alt="logo" src={logo} />
-            </picture>
-          </Link>
-        </div>
+        {logo && (
+          <div className="w-1/4">
+            <Link to={siteUrl}>
+              <picture>
+                <img className="h-16 w-16 rounded-full" alt="logo" src={logo} />
+              </picture>
+            </Link>
+          </div>
+        )}
         <div className="flex w-3/4 items-center justify-end text-right">
           {navLinks.map((n) => (
             <a
